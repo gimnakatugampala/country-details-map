3ebf0e42167b0f71a84d9288cbfae6c75eac4f7a
gimnakatugampala <gimnakatugampala1@gmail.com>
2021-09-09 15:27:55 -0700

    placed the marker

--- src/components/Map/MapContainer.js
+++ src/components/Map/MapContainer.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {GoogleApiWrapper,Map,InfoWindow, Marker, } from 'google-maps-react';
 import mapStyles from './styles'
 
@@ -6,6 +6,29 @@ import mapStyles from './styles'
 
 
 const MapContainer = ({google}) => {
+
+  // Maker State
+  const [state, setState] = useState({
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {},
+
+    // Coordinates of the map
+    mapCenter:{
+      lat: 7.8731,
+      lng: 80.7718
+    }
+  })
+
+
+
+  // Maker Click
+  const onMarkerClick = (props, marker, e) =>
+  setState({
+    selectedPlace: props,
+    activeMarker: marker,
+    showingInfoWindow: true
+  });
 
   // Custom the Map
   const _mapLoaded = (mapProps, map) =>  {
@@ -18,14 +41,33 @@ const MapContainer = ({google}) => {
     <div>
         <Map 
         google={google} 
-        zoom={14} 
+        zoom={5} 
         zoomControl={false}
         mapTypeControl={false}
         fullscreenControl={false}
         keyboardShortcuts={false}
         streetViewControl={false}
         onReady={(mapProps, map) => _mapLoaded(mapProps, map)}
+        initialCenter={{
+          lat:state.mapCenter.lat,
+          lng:state.mapCenter.lng
+        }}
+      
           >
+
+            {/* Marker */}
+        <Marker
+         position={{
+          lat:state.mapCenter.lat,
+          lng:state.mapCenter.lng
+        }} 
+
+        center={{
+          lat:state.mapCenter.lat,
+          lng:state.mapCenter.lng
+        }}
+
+        />
 
         </Map>
     </div>
