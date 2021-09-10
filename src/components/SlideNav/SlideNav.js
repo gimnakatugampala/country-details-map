4df67486607e3b9678327ca6078393388dbccbc6
gimnakatugampala <gimnakatugampala1@gmail.com>
2021-09-10 02:02:11 -0700

    details UI

--- src/components/SlideNav/SlideNav.js
+++ src/components/SlideNav/SlideNav.js
@@ -11,11 +11,11 @@ const SlideNav = ({details}) => {
         <div className="details">
             {/* Section one */}
             <div className="section-a">
-            <h3>United States of America (USA)</h3>
-            <h4>Whishington DC</h4>
-            <img src="https://restcountries.eu/data/usa.svg" style={{width:'100px',height:"50px"}} />
-            <ul>
-                <li>Populations : 4000</li>
+            <h3 style={{whiteSpace:'nowrap'}}>United States of America (USA)</h3>
+            <h5>Whishington DC</h5>
+            <img src="https://restcountries.eu/data/usa.svg" style={{width:'100px',height:"50px",margin:'5px'}} />
+            <ul style={{listStyle:'none',padding:'0'}}>
+                <li style={{padding:'5px'}}>Populations : <span style={{color:'#8FC4DD'}}> 4000 </span></li>
                 <li>Region : North America</li>
                 <li>Sub Region : North AMerica</li>
                 <li>Currency: United States Dollar ($)</li>
@@ -25,20 +25,24 @@ const SlideNav = ({details}) => {
 
             {/* Section Two */}
             <div className="section-b">
-                <h4>Languages</h4>
-                <ul>
+                <h4 style={{color:'#8FC4DD'}}>Languages</h4>
+                <ul style={{listStyle:'none'}}>
                     <li>Sinhala</li>
                     <li>Sinhala</li>
                 </ul>
-                <h4>Borders</h4>
-                <ul>
+                <h4 style={{color:'#8FC4DD'}}>Borders</h4>
+                <ul style={{listStyle:'none'}}>
                     <li>CANA</li>
                     <li>MX</li>
                 </ul>
-                <h4>Timezones</h4>
-                <ul>
+                <h4 style={{color:'#8FC4DD'}}>Timezones</h4>
+                <ul style={{listStyle:'none'}}>
                     <li>+5:40</li>
                     <li>+5:400</li>
+                </ul>
+                <h4 style={{color:'#8FC4DD'}}>Organizations</h4>
+                <ul style={{listStyle:'none'}}>
+                    <li>South Asian Association for Regional Cooperation (SAARC)</li>
                 </ul>
             </div>
 
